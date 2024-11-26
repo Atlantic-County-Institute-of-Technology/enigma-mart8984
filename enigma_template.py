552e840830b27bd404140a8b18b4674da068ad4a
DeepFriedAirFryer <127509834+mart8984@users.noreply.github.com>
2024-11-26 10:47:41 -0500

    Thingy Thangs

--- enigma_template.py
+++ enigma_template.py
@@ -9,22 +9,36 @@ import random
 # we'll be using this string for the majority of our translations
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
+
 # user inputs a message and selects a key (or random), the message is then translated using the cipher
 def encode_message():
-    pass
+    message = input("Please enter the message you would like to encode: ").lower()
+    key = int(input("Enter a number from 1 to 26."))
+    encode_message = ""
+    for char in message:
+        try:
+            addAlph = alphabet.index(char)
+        except:
+            encode_message += char
+        else:
+            addAlph = (addAlph + key) % 26
+            encode_message += alphabet[addAlph]
+    print(encode_message)
 
 # encodes a target file, similarly to encode_message, except now targeting a filename
 def encode_file():
     pass
+
 
 # decodes target file using a user-specified key. If key is unknown, a keypress should
 # call decode_unknown_key()
 def decode_file():
     pass
 
+
 # runs if the key is unknown. If this is true, print out all possible decoding combinations.
 def decode_unknown_key(filename):
-   pass
+    pass
 
 
 # main method declaration
@@ -50,6 +64,7 @@ def main():
             exit()
         else:
             print("Invalid choice. Please try again.")
+
 
 # runs on program start
 if __name__ == "__main__":
